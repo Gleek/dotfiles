3e1833d63c6d38c1462f2c43c9cec6c97a153d65
Umar Ahmad <Gleek@users.noreply.github.com>
2020-08-04 21:05:23 +0530

    add UTF-8 support for emoji chooser

--- choose/choose/emoji.sh
+++ choose/choose/emoji.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-
+export LANG=en_US.UTF-8
 chosen=$(grep -v "#" ~/.config/choose/emojidata | ~/.config/choose/choose false)
 
 [ "$chosen" != "" ] || exit
 
 # c=$(echo "$chosen" | sed "s/[\t ].*//") # cut -c 1
 c=$(echo "$chosen"|awk '{print $1};')
-echo "$c" | tr -d '\n' | pbcopy
-
+echo "$c"| pbcopy
 osascript -e "display notification \"$c copied to clipboard.\" with title \"Emoji copied\""
-
